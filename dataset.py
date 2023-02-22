90c9f63ab10f614cfa7ecd27cb592219646211e2
Victor Mattos <victormattos@poli.ufrj.br>
2023-02-22 16:22:54 -0300

    bug fix +

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import os
+import random
 import tarfile
 import requests
 from PIL import Image
@@ -17,33 +18,46 @@ def get_mnist():
     file.extractall('./dataset')
     file.close()
 
-def read_mnist(mode:str, p: float):
-    dataset_folder = os.path.join("./dataset/mnist_png", mode)
-    x = []
-    y = []
+
+
+def read_dataset(mode:str, p: float):
     
-    for i in range(2):
-        dir = os.path.join(dataset_folder, str(i))
-        print(dir)
+    dataset_folder = os.path.join("./dataset/mnist_png", mode)
+    x_y = []
+
+    for y_i in range(10):
+        dir = os.path.join(dataset_folder, str(y_i))
         x_size_i = len(os.listdir(dir))
         count = 0
         selected_images = round(p*x_size_i)
+        
         for j in os.listdir(dir):
             image = Image.open(os.path.join(dir, j))
             image = image.convert('L')
             width, height = image.size
             pixel_data = []
+            
             for h in range(height):
                 row_data = []
                 for w in range(width):
                     pixel_value = image.getpixel((w, h))
                     row_data.append(pixel_value)
                 pixel_data.append(row_data)
-            x.append(pixel_data)
-            y.append(i)
+            x_y.append((pixel_data, y_i))
             count += 1 
+            
             if count == selected_images:
                 break
-    return x, y
 
-x, y = read_mnist("testing", 0.01)
+    return x_y
+
+def shuffle_dataset(x_y):
+    random.shuffle(x_y)
+    x = []
+    y = []
+
+    for i, j in x_y:
+        x.append(i)
+        y.append(j)
+
+    return x, y
